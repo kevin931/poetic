d5ec63a400c398ad6d742f2c3ddd37e72faf2eab
Kevin Wang <kevinwang@smu.edu>
2020-04-22 14:15:07 -0500

    Changed the directory for file paths

--- Scripts/poetry_predictor.py
+++ Scripts/poetry_predictor.py
@@ -86,12 +86,12 @@ class Initialize():
         return word_dictionary
 
     def load_model(self):
-        json_file = open('./Models/sent_model.json', 'r')
+        json_file = open('../Models/sent_model.json', 'r')
         loaded_model_json = json_file.read()
         json_file.close()
         sent_model = keras.models.model_from_json(loaded_model_json)
         # load weights into new model
-        sent_model.load_weights("./Models/sent_model.h5")
+        sent_model.load_weights("../Models/sent_model.h5")
 
         return sent_model
 
