f821f30ee39516829ef6db24990035c533dd6404
Kevin Wang <kevinwang@smu.edu>
2020-05-18 20:58:47 -0500

    Modified file path for the new launcher script

--- Lib/poetry_predictor.py
+++ Lib/poetry_predictor.py
@@ -70,16 +70,16 @@ class GUI:
 class Initialize():
 
     def load_dict(self):
-        word_dictionary = gs.corpora.Dictionary.load_from_text(fname="../Models/word_dictionary_complete.txt")
+        word_dictionary = gs.corpora.Dictionary.load_from_text(fname="./Models/word_dictionary_complete.txt")
         return word_dictionary
 
     def load_model(self):
-        json_file = open('../Models/sent_model.json', 'r')
+        json_file = open('./Models/sent_model.json', 'r')
         loaded_model_json = json_file.read()
         json_file.close()
         sent_model = keras.models.model_from_json(loaded_model_json)
         # load weights into new model
-        sent_model.load_weights("../Models/sent_model.h5")
+        sent_model.load_weights("./Models/sent_model.h5")
 
         return sent_model
 
