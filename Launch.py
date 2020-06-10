f0c23dcd68782af5f6a1accf58ad3f50846a7606
Kevin Wang <kevinwang@smu.edu>
2020-06-09 19:11:33 -0500

    Updated to work with the separate files

--- Launch.py
+++ Launch.py
@@ -1,13 +1,16 @@
-from Lib import poetry_predictor as pp
+## Import necessary module
+from Lib import *
 
 def main():
 
     ## Initialize and load model
-    new_program = pp.Initialize()
+    new_program = Initialize.Initialize()
     model = new_program.load_model()
     dict = new_program.load_dict()
-
-    newGUI = pp.GUI(model, dict)
+    ## Initialize the predictor
+    predictor = Predictor.Predictor(model, dict)
+    ## Start the program with the GUI
+    newGUI = GUI.GUI(predictor)
 
 
 if __name__ == "__main__":
