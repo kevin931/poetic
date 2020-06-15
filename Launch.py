22727fe541e8760606a1cf6635277bf39537a104
Kevin Wang <kevinwang@smu.edu>
2020-06-15 18:37:09 -0500

    Add back command-line mode with flags support

--- Launch.py
+++ Launch.py
@@ -1,17 +1,26 @@
-## Import necessary module
+# Import necessary module
 from Lib import *
 
 def main():
 
-    ## Initialize and load model
-    new_program = Initialize.Initialize()
-    model = new_program.load_model()
-    dict = new_program.load_dict()
+    ## Initialize and load the program
+    args, model, dict = Initialize.Initializer.initialize()
+
+    # print(args)
+
     ## Initialize the predictor
     predictor = Predictor.Predictor(model, dict)
-    ## Start the program with the GUI
-    newGUI = GUI.GUI(predictor)
 
+    ## Check for "-s" tag
+    if args["Sentence"] is not None:
+        ## Prediction
+        score = predictor.predict(args["Sentence"])
+        score = str(round(score, 2))
+        print("\n\nYour score is " + score + "\n\n")
+
+    ## Start the program with the GUI
+    if args["GUI"] or args["Sentence"] is None:
+        newGUI = GUI.GUI(predictor)
 
 if __name__ == "__main__":
     main()
