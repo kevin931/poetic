0775c65f274ed3eaf06cfeb771ef4ad9bc809ad9
Kevin Wang <kevinwang@smu.edu>
2020-06-15 18:36:41 -0500

    Add command line parsing and modified Initializer

--- Lib/Initialize.py
+++ Lib/Initialize.py
@@ -5,19 +5,48 @@
 ## Import necessary module
 from tensorflow import keras
 import gensim as gs
+import argparse
 
-class Initialize():
+class Initializer():
 
-    def load_dict(self):
+    @classmethod
+    def initialize(cls):
+        ## Command-line arguments arguments
+        arguments = Arguments()
+        arguments = arguments.parse()
+        arguments = vars(arguments)
+
+        ## Load dictionary and model
+        model = cls.load_model()
+        dict = cls.load_dict()
+
+        return arguments, model, dict
+
+    @staticmethod
+    def load_dict():
         word_dictionary = gs.corpora.Dictionary.load_from_text(fname="./Models/word_dictionary_complete.txt")
         return word_dictionary
 
-    def load_model(self):
+    @staticmethod
+    def load_model():
         json_file = open('./Models/sent_model.json', 'r')
         loaded_model_json = json_file.read()
         json_file.close()
         sent_model = keras.models.model_from_json(loaded_model_json)
         # load weights into new model
         sent_model.load_weights("./Models/sent_model.h5")
-
         return sent_model
+
+## Parsing arguments
+class Arguments():
+    ## Constructor
+    def __init__(self):
+        ## New Parser
+        self.parser = argparse.ArgumentParser(description="Parse command line arguments")
+        self.parser.add_argument("-g", "--GUI", action="store_true")
+        self.parser.add_argument("-s", "--Sentence", action="store")
+        self.parser.add_argument("--version", action="version", version="Poetry Predictor 0.2.0 alpha")
+
+    def parse(self):
+        arguments = self.parser.parse_args()
+        return arguments
