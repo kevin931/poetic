36782299a05871e90b6cb6dc7635e953cc2038b1
Kevin Wang <kevinwang@smu.edu>
2020-06-21 18:02:43 -0500

    Added support for command line for processing text files.
    The logic for launching GUI stays the same with some slight changes to 
    implementation. Logics are implemented to efficiently process "-s", 
    "-f", and "-o" tag.

--- Launch.py
+++ Launch.py
@@ -6,32 +6,38 @@ def main():
     ## Initialize and load the program
     args, model, dict = Initialize.Initializer.initialize()
 
-    # print(args)
 
     ## Initialize the predictor
     predictor = Predictor.Predictor(model, dict)
 
-    ## Check for "-s" tag
-    if args["Sentence"] is not None:
-        ## Prediction
-        score = predictor.predict(args["Sentence"], type="Content")
+    ## Check for command-line mode
+    if args["Sentence"] is not None or args["File"] is not None:
 
-    ## Check for "-f" tag
-    if args["File"] is not None:
-        ## Prediction
-        score = predictor.predict(args["Sentence"], type="Path")
+        ## Check for "-s" tag
+        if args["Sentence"] is not None:
+            ## Prediction
+            score = predictor.predict(args["Sentence"], type="Content")
 
-    ## Diagnostics
-    score = Diagnostics.Diagnostics.five_number(score)
-    ## Extracting the median
-    score = str(round(score["Median"], 2))
-    print("\n\nYour score is " + score + "\n\n")
+        ## Check for "-f" tag
+        if args["File"] is not None:
+            ## Prediction
+            score = predictor.predict(args["Sentence"], type="Path")
+
+        ## Run diagnostics
+        score.run_diagnostics()
+
+        ## Check for "-o" tag
+        if args["Out"] is not None:
+            score.to_file(args["Out"])
+        else:
+            print(score.generate_report())
+
+    ## Check whether for default GUI mode without "-f" and "-s" tag
+    launch_GUI = True if args["Sentence"] is None and args["File"] is None else False
 
     ## Start the program with the GUI
-    if args["GUI"] or args["Sentence"] is None:
+    if args["GUI"] or launch_GUI:
         newGUI = GUI.GUI(predictor)
 
 if __name__ == "__main__":
     main()
-
-predictor.predict("I am a good man. Hi there")
