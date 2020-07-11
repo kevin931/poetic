7e3c819be7aee90879ce4b0aaeb2d767a6cf7cd0
Kevin Wang <kevinwang@smu.edu>
2020-07-11 18:03:18 -0500

    Added docstrings
    Docstrings are added for the Predictor class and the planned public 
    methods. Some of the methods will become private in future commits.

--- poetry_predictor/predictor.py
+++ poetry_predictor/predictor.py
@@ -1,81 +1,131 @@
-###################
-##### Predict inuputs
-###################
-
-## Load necessary modules
 from tensorflow import keras
 from nltk.tokenize import word_tokenize, sent_tokenize
 from poetry_predictor.results import Diagnostics
 
 class Predictor():
+    """
+    This is the class to process and predict inputs.
 
-    ## Constructor
+    Attributes:
+        model:The pre-trained keras model.
+        dict: Gensim dictionary for word IDs.
+
+    Methods:
+        predict(input, type=["Content", "Path"])
+            Predicts the poetic score of the given input.
+        preprocess(input)
+            Preprocesses (tokenize, lower case, and pad) the given input.
+        tokenize(input)
+            Word-tokenizes the given input.
+        word_id(input)
+            Converts words from tokenized input to indices.
+    """
+
     def __init__(self, model=None, dict=None):
+        """
+        Parameters:
+            model: keras model, optional
+                The pre-trained Keras model used to predict poetic scores.
+            dict: gensim dictionary, optional
+                The dictionary used to convert words to indices.
+        """
         self.model = model
         self.dict = dict
 
     def predict(self, input, type=["Content", "Path"]):
+        """
+        Predict poetic score from file or direct input.
 
-        ## Check for file path input
+        Parameters:
+            input (str): Text content or file path.
+            type (str): "Content" or "Path" for text or file.
+
+        Returns:
+            score (float): The predicted scores of given input.
+        """
+
         if type == "Path":
-            # Load the file
             input = self.file_load(input)
 
-        ## Preprocess the input
         input = self.proprocess(input)
-        ## Prediction
         results = self.model.predict(input)
         results = results.tolist()
         score = _Predictions(results, self._sentences)
 
         return score
 
-    ## Preprocess the input
     def proprocess(self, input):
-        ## Tokenize
+        """
+        Preprocess inputs: tokenize, to lower, and padding.
+
+        Parameters:
+            input (str):
+                Text either in a single string or a list of strings.
+
+        Returns:
+            sent_test (list): A 2-d list of processed inputs.
+        """
+
         sent_token = self.tokenize(input)
-        ## Check for errors
+
         self.check_requirement(sent_token)
-        ## To lower case
+
         sent_lower = []
         for sentence in sent_token:
             word_lower = [word.lower() for word in sentence]
             sent_lower.append(word_lower)
-        ## Word to ID
+
         id_sent = self.word_id(sent_lower)
-        ## Padding
+
         sent_test = keras.preprocessing.sequence.pad_sequences(id_sent, maxlen=456)
 
         return sent_test
 
-    ## Preprocess files
     def file_load(self, path):
-        # Open and read file
+        # Open a specified file.
+        # Method used for accepting file input.
+
         file = open(path, "r", encoding='utf-8')
         file = file.read()
 
         return file
 
-    ## Sentence and word tokenize
     def tokenize(self, input):
-        ## Split into sentences first
+        """
+        Tokenize text input into sentences and then words.
+
+        Parameters:
+            input (str): A string or list of strings of text.
+
+        Returns:
+            tokens (list): A 2-d list of tokenized words.
+        """
+
+        # Sentence tokenization
         sentences = sent_tokenize(input)
-        ## Store tokenized sentence with the class
         self._sentences = sentences
-        ## Word tokenize each sentence
+
+        # Word tokenize
         tokens = []
         for sentence in sentences:
             words = word_tokenize(sentence)
             tokens.append(words)
         return tokens
 
-    ## Word to index
-    def word_id(self, input):
 
-        ## Sentence First
+    def word_id(self, input):
+        """
+        Convert tokenized words to word IDs using a gensim dictionary.
+
+        Parameters:
+            input (list): A 2-d list of tokenized words.
+
+        Returns:
+            id_input (list): A 2-d list of word ids.
+        """
+
         id_input = []
         for sentence in input:
-            ## Each word in sentence
             id_sent = []
             for word in sentence:
                 try:
@@ -83,27 +133,27 @@ class Predictor():
                     id_sent.append(self.dict.token2id.get(word))
                 except:
                     id_sent.append(0)
-            ## Append sentences
             id_input.append(id_sent)
 
         return(id_input)
 
-    ## Check Input length requirement
     def check_requirement(self,input):
-        ## Check empty input
+        #Check empty input
         if len(input)==0:
             raise InputLengthError()
 
-    ## Input length out of bound
+
     class InputLengthError(Exception):
+        # Raise error for inproper input length
         def __init__(self):
             message = "Input length out of bound: must be between 1 and 465"
             super().__init__(message)
 
-## Class for Predictor outputs, inheriting from Diagnostics
+
 class _Predictions(Diagnostics):
+    # Class for Predictor outputs
+    # Inheriting from Diagnostics class
+
     def __init__(self, results, sentences):
-        ## Process the results into one single list
         results = [prediction[0] for prediction in results]
-        ## Call Diagnostics class constructor
         super().__init__(predictions=results, sentences=sentences)
