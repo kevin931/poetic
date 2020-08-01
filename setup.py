4383e3fa1e02178442c8df165da025886e7e8331
Kevin Wang <kevinwang@smu.edu>
2020-08-01 10:23:07 -0500

    Init commit.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name = "poetic",
+    version = "1.0.0-Alpha",
+    url = "https://github.com/kevin931/poetic",
+    author = "Kevin Wang",
+    author_email = "bridgemarian@gmail.com",
+    description = "Predicts how poetic sentences are.",
+    long_description = open("README.md").read(),
+    packages=["poetic"],
+    install_requires = ["tensorflow>=2.1",
+                        "gensim>=1.18",
+                        "nltk>=3.8"
+    ],
+    data_files = [("data", ["word_dictionary_complete.txt"])],
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3 :: Only"
+    ]
+)
