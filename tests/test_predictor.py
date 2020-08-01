5d5ec19f2f2bb576ee87d0a292788b05ec0c0bc6
Kevin Wang <kevinwang@smu.edu>
2020-08-01 11:12:49 -0500

    Init commit.
    A skeleton of this test script is set. More actual test logic to come.

---
+++ tests/test_predictor.py
@@ -0,0 +1,16 @@
+import unittest
+from ..poetic.predictor import Predictor
+
+class TestPredictor(unittest.TestCase):
+    # Class to test the Predictor Class
+
+    # pred = Predictor()
+
+    def test_constructor_model(self):
+        pass
+
+    def test_file_load(self):
+        pass
+
+    def test_word_id(self):
+        pass
