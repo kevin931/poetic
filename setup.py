055d38d94788c80f01bdd6269fdd9dc4607eb554
Kevin Wang <kevinwang@smu.edu>
2020-08-01 15:57:57 -0500

    Various updates.
    Updated the file included, version number, and the long descriptions 
    pointing to Github.

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 import setuptools
 
+description = "For documentation and detials, "
+description += "please refer to http://github.com/kevin931/poetic"
+
 setuptools.setup(
     name = "poetic-py",
-    version = "1.0.0-Beta",
+    version = "1.0.0b1",
     url = "https://github.com/kevin931/poetic",
     author = "Kevin Wang",
     author_email = "bridgemarian@gmail.com",
     description = "Predicts how poetic sentences are.",
-    long_description = open("README.md").read(),
+    long_description = description,
     packages=["poetic"],
     install_requires = ["tensorflow>=2.1",
-                        "gensim>=1.18",
-                        "nltk>=3.8"
+                        "gensim>=3.8",
+                        "nltk>=3.4"
     ],
-    data_files = [("data", ["word_dictionary_complete.txt"])],
+    install_package_data=True,
+    package_data={"poetic":["*.txt"]},
     classifiers = [
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3 :: Only"
