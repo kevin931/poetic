f795293218b5606127420db0c21cbca24e4762fa
Kevin Wang <kevinwang@smu.edu>
2020-08-01 10:41:38 -0500

    Package docstring added.

---
+++ poetic/__init__.py
@@ -0,0 +1,8 @@
+"""Poetic Package.
+
+This is the Poetic package, which provides functionalities for
+predicting how poetic language is using a Keras model trained on
+public-domain works. For further documentation and latest releases,
+please visit https://github.com/kevin931/poetic.
+
+"""
