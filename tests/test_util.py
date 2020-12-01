fa0320338145a7b08e81dce1a4db62b15106e648
Kevin Wang <kevinwang@smu.edu>
2020-11-30 20:29:42 -0600

    Added tests for Initializer class.
    The following methods are added:
    test_load_dict, test_check_assets_type, test_check_assets_contents_type.
    For the Initializer class, the load_model, Initializer, and
    download_assets methods are untestable in this version.

--- tests/test_util.py
+++ tests/test_util.py
@@ -2,6 +2,8 @@ from poetic.util import Info, Initializer
 import poetic
 
 import re
+import os
+import gensim
 
 class TestInfo():
     
@@ -28,4 +30,29 @@ class TestInfo():
         assert matched is not None
         
         
+class TestInitializer():
+    
+    @classmethod
+    def setup_class(cls):
+        # File paths
+        cls.script_path = os.path.dirname(os.path.realpath(__file__))
         
+    
+    def test_load_dict(self):
+        gensim_dict = Initializer.load_dict()
+        assert isinstance(gensim_dict, gensim.corpora.dictionary.Dictionary)
+    
+    
+    def test_check_assets_type(self):
+        status = Initializer.check_assets()
+        assert isinstance(status, dict)
+        
+        
+    def test_check_assets_contents_type(self):
+        status = Initializer.check_assets()
+        status_all = isinstance(status["all_exist"], bool)  
+        status_model = isinstance(status["model"], bool) 
+        status_weights = isinstance(status["weights"], bool)
+        
+        status_bool = status_all and status_model and status_weights
+        assert status_bool
