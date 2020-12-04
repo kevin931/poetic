7a7267f7c37f6cb9353d98732818e98375a51947
Kevin Wang <kevinwang@smu.edu>
2020-12-03 19:23:25 -0600

    Test cases added.
    The error handlings of the to_csv() and to_file() methods are added. The
    generate_report() method's string is also explicitly
    tested instead of just the return type. The truncation funtionality of
    the string representation is tested as well.

--- tests/test_results.py
+++ tests/test_results.py
@@ -1,8 +1,12 @@
 from poetic.results import Diagnostics
+from poetic.util import Info
 
 import pytest
 from math import isclose
 import os
+from io import StringIO
+import sys
+import csv
 
 
 class TestDiagnostics():
@@ -25,6 +29,14 @@ class TestDiagnostics():
         output = str(self.results)
         expected = "Diagnostics object for the following predictions: [1, 0, 1, 0]"
         assert output == expected
+        
+        
+    def test_str_long_predictions(self):
+        pred_long = [0]*15
+        results_long = Diagnostics(pred_long)
+        output = str(results_long)
+        output = output[len(output)-3:]
+        assert output == "..."
         
                 
     def test_repr_type(self):
@@ -102,14 +114,106 @@ class TestDiagnostics():
         self.results.to_file(path)       
         assert os.path.exists(path)
         
-             
+
+    def test_to_csv_error_message(self):
+        nonexistant_path = "./nonexistant/a.txt"
+        screen_stdout = sys.stdout
+        string_stdout = StringIO()
+        sys.stdout = string_stdout
+        
+        try:
+            self.results.to_csv(nonexistant_path)
+        except:
+            output = string_stdout.getvalue()
+        finally:
+            sys.stdout = screen_stdout
+            
+        expected = "Warning: Unable to open file at designated path."
+        assert expected in output
+        
+        
+    def test_to_file_error_message(self):
+        nonexistant_path = "./nonexistant/a.txt"
+        screen_stdout = sys.stdout
+        string_stdout = StringIO()
+        sys.stdout = string_stdout
+        
+        try:
+            self.results.to_file(nonexistant_path)
+        except:
+            output = string_stdout.getvalue()
+        finally:
+            sys.stdout = screen_stdout
+            
+        expected = "Warning: Unable to open file at designated path."
+        assert expected in output
+        
+        
+    def test_to_csv_exception_handling(self):       
+        nonexistant_path = "./nonexistant/a.txt"
+        try:
+            self.results.to_csv(nonexistant_path)
+        except Exception as e:
+            assert isinstance(e, Exception)
+        else:
+            assert False
+            
+            
+    def test_to_file_exception_handling(self):      
+        nonexistant_path = "./nonexistant/a.txt"
+        try:
+            self.results.to_file(nonexistant_path)
+        except Exception as e:
+            assert isinstance(e, Exception)
+        else:
+            assert False
+            
+            
+    def test_to_csv_sentences(self):
+        path = self.script_path + "/data/csv_test_sentence_temp.csv"
+        self.results.sentences = ["This", "is", "a", "test"]
+        self.results.to_csv(path)
+        
+        f = open(path, newline="")
+        contents = f.read()
+        f.close()
+        
+        for sentence in self.results.sentences:
+            if sentence not in contents:
+                assert False
+            
+        assert True
+        
+                 
     def test_generate_report_type(self):
         report = self.results.generate_report()
         assert isinstance(report, str)
         
-    
+        
+    def test_generate_report_contents(self):
+        report = self.results.generate_report()
+        
+        contents = []
+        contents.append(Info.version())
+        contents.append(str(self.results.diagnostics["Sentence_count"]))
+        
+        five_number_contents = list(self.results.diagnostics["Five_num"].values())
+        five_number_contents = [str(item) for item in five_number_contents]
+        contents += five_number_contents
+        
+        predictions = [str(pred) for pred in self.results.predictions]
+        contents += predictions
+        
+        for item in contents:
+            if item not in report:
+                assert False
+                
+        assert True
+        
+           
     @classmethod   
     def teardown_class(cls):
         os.remove(cls.script_path + "/data/csv_test_temp.csv")
         os.remove(cls.script_path + "/data/txt_test_temp.txt")
         os.remove(cls.script_path + "/data/csv_test_via_txt_temp.txt")
+        os.remove(cls.script_path + "/data/csv_test_sentence_temp.csv")
