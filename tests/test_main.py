881a7d33ab39e2a932b3b82d4c8b5cb879183be3
Kevin Wang <kevinwang@smu.edu>
2020-12-06 10:53:55 -0600

    Disabled pylint for no return
    The test case test_main_return_none checks for return tupe none. Thus, pylint is disabled for none assignment.

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,5 +4,5 @@ from poetic.__main__ import main
 class TestMain():
     
     def test_main_return_none(self):
-        result = main(_test=True)
+        result = main(_test=True) #pylint: disable=assignment-from-no-return
         assert result is None
