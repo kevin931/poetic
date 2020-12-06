801e328918c5b09bb9c9bae382870389f47f1351
Kevin Wang <kevinwang@smu.edu>
2020-12-05 20:22:34 -0600

    Added docstring details
    The docstring now list modules and package-level classes.

--- poetic/__init__.py
+++ poetic/__init__.py
@@ -5,6 +5,15 @@ predicting how poetic language is using a Keras model trained on
 public-domain works. For further documentation and latest releases,
 please visit https://github.com/kevin931/poetic.
 
+Modules:
+    - predictor
+    - results
+    - util
+    
+Package-level Classes:
+    - Predictor
+    - Diagnostics
+    
 """
 
 from poetic.predictor import Predictor
