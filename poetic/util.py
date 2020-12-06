7e9de19642042479a6aa9f71cde8224da73d73a4
Kevin Wang <kevinwang@smu.edu>
2020-12-05 20:17:16 -0600

    Added testing parameters.
    The test cases now have _test parameter as necessary for unit testing purposes.

--- poetic/util.py
+++ poetic/util.py
@@ -23,6 +23,7 @@ import pkg_resources
 
 from poetic import exceptions
 
+
 class Info():
     """
     Provides the basic information of the package.
@@ -44,6 +45,7 @@ class Info():
         VERSION = "1.0.0b1"
         return VERSION
 
+
     @staticmethod
     def build_status():
         """
@@ -55,6 +57,7 @@ class Info():
 
         BUILD = "Dev"
         return BUILD
+
 
 class Initializer():
     """Initializes core components of the package.
@@ -84,8 +87,9 @@ class Initializer():
     _weights_dir = _data_dir+"sent_model.h5"
     _model_dir = _data_dir+"sent_model.json"
 
+
     @classmethod
-    def initialize(cls):
+    def initialize(cls, _test=False):
         """Initializes the package.
 
         This methods checks for any command line arguments,
@@ -101,18 +105,19 @@ class Initializer():
                 A gensim dictionary.
 
         """
-
+        
+        test_arguments = "" if _test else None
         arguments = _Arguments()
-        arguments = arguments.parse()
+        arguments = arguments.parse(test_arguments)
 
-        model = cls.load_model()
+        model = cls.load_model(_test=True)
         word_dictionary = cls.load_dict()
 
         return arguments, model, word_dictionary
 
+
     @classmethod
     def load_dict(cls):
-
         """Loads gensim dictionary.
 
         Returns:
@@ -120,12 +125,14 @@ class Initializer():
                 A gensim dictionary.
 
         """
+        
         dir = cls._data_dir + "word_dictionary_complete.txt"
         word_dictionary = gs.corpora.Dictionary.load_from_text(fname=dir)
         return word_dictionary
 
+
     @classmethod
-    def load_model(cls, force_download=False):
+    def load_model(cls, force_download=False, _test=False):
         """Load Keras models.
 
         This method uses Keras interface to load the previously
@@ -143,19 +150,24 @@ class Initializer():
             model (tensorflow.python.keras.engine.training.Model): Pretrained Keras model
 
         """
-
+        
         # Check model assets status
         assets = cls.check_assets()
         if not assets["all_exist"]:
+            assets["all_exist"] = True if _test else False
             cls.download_assets(assets_status=assets, force_download=force_download)
+            
+        model_dir = cls._test_variables()["model"] if _test else cls._model_dir
+        weights_dir = cls._test_variables()["weights"] if _test else cls._weights_dir
 
-        json_file = open(cls._model_dir, 'r')
+        json_file = open(model_dir, 'r')
         loaded_model_json = json_file.read()
         json_file.close()
         model = keras.models.model_from_json(loaded_model_json)
         # load weights into new model
-        model.load_weights(cls._weights_dir)
+        model.load_weights(weights_dir)
         return model
+
 
     @classmethod
     def check_assets(cls):
@@ -180,8 +192,9 @@ class Initializer():
 
         return status
 
+
     @classmethod
-    def download_assets(cls, assets_status=None, force_download=False):
+    def download_assets(cls, assets_status=None, force_download=False, _test=False, _test_input=None):
         """Method to download models.
 
         This method downloads models from the poetic-models
@@ -213,7 +226,8 @@ class Initializer():
 
         if assets_status is None:
             assets_status = cls.check_assets()
-        # Check if download needed
+            if _test and _test_input is None: assets_status["all_exist"] = True
+        
         if assets_status["all_exist"]:
             return None
 
@@ -226,7 +240,7 @@ class Initializer():
         print(message)
 
         if not force_download:
-            value = input("Would you like to download? [y/n]")
+            value = input("Would you like to download? [y/n]") if _test_input is None else _test_input
             # Anything other than "y"
             if value.lower() != "y":
                 message_2 = "\nYou have declined to download the assets.\n"
@@ -245,12 +259,24 @@ class Initializer():
         message_3 += "This may take quite a while, "
         message_3 += "go grab a coffee and be poetic.\n"
         print(message_3)
-
+        
         with urlopen(url) as contents:
+            if _test: return contents
             contents = contents.read()
             with ZipFile(BytesIO(contents)) as file:
                 file.extractall(cls._data_dir)
-
+                
+    
+    @classmethod
+    def _test_variables(cls):
+        # Test models for unit testing
+        
+        module_path = path.dirname(path.realpath(__file__))
+        test_model_path = module_path + "/../tests/data/lexical_model_dummy.json"
+        test_weights_path = module_path + "/../tests/data/lexical_model_dummy.h5"
+        
+        return_dict = {"model": test_model_path, "weights": test_weights_path}
+        return return_dict
 
 
 class _Arguments():
@@ -270,6 +296,7 @@ class _Arguments():
                                  help="Path to save results.")
         self.parser.add_argument("--version", action="version", version=self.version())
 
+
     def parse(self, args=None):
         # Parse arguments
 
@@ -283,6 +310,7 @@ class _Arguments():
             raise exceptions.UnsupportedConfigError(message)
 
         return arguments
+
 
     def version(self):
         # Format the command-line version output
