56af4c08baaadc431c7ea3747151b80e9c11b5d7
Kevin Wang <kevinwang@smu.edu>
2020-12-05 20:21:56 -0600

    Modified for testing
    The docstring is improved for clarity. All codes now under the main function for unit testing.

--- poetic/__main__.py
+++ poetic/__main__.py
@@ -1,8 +1,9 @@
-"""Runs package as main.
+"""Runs package as a program through main.
 
 The __main__.py is intended for the command line usage
 of the package with the command below. Command line arguments
-are parsed accordingly with its associated behaviors.
+are parsed accordingly with its associated behaviors. For doccumnentation,
+please visit: poetic.readthedocs.io or github.com/kevin931/poetic
 
     $python -m poetic
 
@@ -10,28 +11,33 @@ are parsed accordingly with its associated behaviors.
 
 from poetic import gui, predictor, util, results
 
-# Initialization and predictor
-args, model, dict = util.Initializer.initialize()
-new_pred = predictor.Predictor(model, dict)
+def main(*, _test=False):
+    # Initialization and predictor
+    args, model, dictionary = util.Initializer.initialize(_test=_test)
+    new_pred = predictor.Predictor(model, dictionary)
 
-# Check for command-line mode
-if args["Sentence"] is not None or args["File"] is not None:
-    # "-s" flag
-    if args["Sentence"] is not None:
-        score = new_pred.predict(args["Sentence"])
-    # "-f" flag
-    if args["File"] is not None:
-        score = new_pred.predict_file(args["File"])
+    # Check for command-line mode
+    if args["Sentence"] is not None or args["File"] is not None:
+        # "-s" flag
+        if args["Sentence"] is not None:
+            score = new_pred.predict(args["Sentence"])
+        # "-f" flag
+        if args["File"] is not None:
+            score = new_pred.predict_file(args["File"])
 
-    score.run_diagnostics()
-    # Check for "-o" tag
-    if args["Out"] is not None:
-        score.to_file(args["Out"])
-    else:
-        print(score.generate_report())
+        score.run_diagnostics()
+        # Check for "-o" tag
+        if args["Out"] is not None:
+            score.to_file(args["Out"])
+        else:
+            print(score.generate_report())
 
-# Check for default GUI mode without "-f" and "-s" tag
-launch_GUI = True if args["Sentence"] is None and args["File"] is None else False
-# Start the program with the GUI
-if args["GUI"] or launch_GUI:
-    newGUI = gui.GUI(new_pred)
+    # Check for default GUI mode without "-f" and "-s" tag
+    launch_GUI = True if args["Sentence"] is None and args["File"] is None else False
+    # Start the program with the GUI
+    if (args["GUI"] or launch_GUI) and not _test:
+        gui.GUI(new_pred)
+
+
+if __name__ == "__main__":
+    main()
