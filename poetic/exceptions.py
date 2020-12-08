e87bc677b23076ca3527aaf4a2986f5b80aaacca
Kevin Wang <kevinwang@smu.edu>
2020-12-07 20:30:37 -0600

    Added docstring
    The docstring is added to conform with other files.

--- poetic/exceptions.py
+++ poetic/exceptions.py
@@ -1,8 +1,25 @@
+""" Module for custom exceptions.
+
+The exceptions module includes custom classes for specific
+errors in the poetic package. This is not part of the public
+interface.
+
+"""
+
+
 from typing import Optional
 
 class InputLengthError(Exception):
-    # Raise error for inproper input length
-    # Used in Predictor class.
+    
+    """ Raises Input Length Error.
+    
+    This exception is used in the Predictor class for the input length
+    of 0.
+    
+    Args:
+        message(str): The error message to display. 
+    
+    """
 
     def __init__(self, message: Optional[str]=None) -> None:
         if message is None:
@@ -11,8 +28,16 @@ class InputLengthError(Exception):
 
 
 class UnsupportedConfigError(Exception):
-    # Check unsupported command line argument configuration.
-    # Used in util.Arguments()
+    
+    """ Raises Unsupported Configuration Error.
+    
+    This exception is used in the _Arguments class for checking
+    unsupported commandline flags.
+    
+    Args:
+        message(str): The error message to display. 
+    
+    """
 
     def __init__(self, message: Optional[str]=None) -> None:
         if message is None:
